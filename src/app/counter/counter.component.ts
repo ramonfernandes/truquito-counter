b38cffbca4f696316eec42a96c89f3ba59b4a935
ramon <ramon.correa@thomsonreuters.com>
2020-12-28 21:54:24 -0300

    Style

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,35 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent implements OnInit {
-
   playerOne = 0;
   playerTwo = 0;
-  playerOneName = "Nós";
-  playerTwoName = "Eles";
+  playerOneName = 'Nós';
+  playerTwoName = 'Eles';
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public sumPlayerOne(): void {
-    this.playerOne++;
+    if (this.playerOne < 30) this.playerOne++;
   }
 
   public sumPlayerTwo(): void {
-    this.playerTwo++;
+    if (this.playerTwo < 30) this.playerTwo++;
   }
 
   public subPlayerOne(): void {
-    this.playerOne--;
+    if (this.playerOne > 0) this.playerOne--;
   }
 
   public subPlayerTwo(): void {
-    this.playerTwo--;
+    if (this.playerTwo > 0) this.playerTwo--;
   }
 
   public reset(): void {
@@ -39,4 +36,3 @@ export class CounterComponent implements OnInit {
     this.playerTwo = 0;
   }
 }
-
